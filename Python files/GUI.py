b72f83d48da07ce3561f3082ed6234bb6bfa48d8
BHASVIC-EthenDoherty23 <145350377+BHASVIC-EthenDoherty23@users.noreply.github.com>
2024-09-29 20:47:04 +0100

    Update GUI.py

--- Python files/GUI.py
+++ Python files/GUI.py
@@ -1,8 +1,27 @@
+import time
+from email.policy import default
+
 import pygame
 
 pygame.init()
-size = pygame.display.get_desktop_sizes()
-screen = pygame.display.set_mode(size[0])
+desktop_size = pygame.display.get_desktop_sizes()
+size = desktop_size[0]
+screen = pygame.display.set_mode(size)
 
-while True:
-    pass
+defaultBackgroundColor = 27,27,237
+defaultBoxColour = 223,223,252
+
+currentScene = ""
+
+def mainMenuDisplayer():
+    background = pygame.draw.rect(screen, defaultBackgroundColor, (0,0,size[0],size[1]), 0)
+    playButton = pygame.draw.rect(screen, defaultBoxColour , (size[0]/2.5 , size[1] * 0.2 , size[0]/4 , size[1]/10), 0 )
+    settingsButton = pygame.draw.rect(screen, defaultBoxColour , (size[0]/2.5 , size[1] * 0.4 , size[0]/4 , size[1]/10), 0 )
+    statisticsButton = pygame.draw.rect(screen, defaultBoxColour , (size[0]/2.5 , size[1] * 0.6 , size[0]/4 , size[1]/10),  0 )
+    quitButton = pygame.draw.rect(screen, defaultBoxColour,  (size[0]/2.5 , size[1] * 0.8 , size[0]/4 , size[1]/10), 0 )
+    currentScene = "mainMenu"
+
+mainMenuDisplayer()
+pygame.display.flip()
+
+time.sleep(60)
