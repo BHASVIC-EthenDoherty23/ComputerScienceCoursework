8f5ff08af9d64eb7f6ac6e5b5fc226cea5d3d85b
BHASVIC-EthenDoherty23 <145350377+BHASVIC-EthenDoherty23@users.noreply.github.com>
2024-10-23 10:00:49 +0100

    backStack

--- Python files/GUI.py
+++ Python files/GUI.py
@@ -13,24 +13,28 @@ screen = pygame.display.set_mode(size)
 
 #load images/backgrounds
 defaultBackgroundColor = 27, 27, 237  #Dark Blue like the ocean
-start_Button = pygame.image.load('Images/StartButton.png')  #loads the StartButton image
-settings_Button = pygame.image.load('Images/settingButton.png')  #loads the SettingsButton image
-statistics_Button = pygame.image.load('Images/statisticButton.png')  #loads the StatisticsButton image
-exit_Button = pygame.image.load('Images/ExitButton.png')  # loads the ExitButton image
+start_Button = pygame.image.load('Images/StartButton.png').convert_alpha()  #loads the StartButton image
+settings_Button = pygame.image.load('Images/settingButton.png').convert_alpha()   #loads the SettingsButton image
+statistics_Button = pygame.image.load('Images/statisticButton.png').convert_alpha()   #loads the StatisticsButton image
+exit_Button = pygame.image.load('Images/ExitButton.png').convert_alpha()   # loads the ExitButton image
+back_Button = pygame.image.load('Images/back.png').convert_alpha()
 
 #create buttons using button class
 startButton = button.Button(size[0] / 2.5, size[1] * 0.2, start_Button)
 settingsButton = button.Button(size[0] / 2.5, size[1] * 0.4, settings_Button)
 statisticsButton = button.Button(size[0] / 2.5, size[1] * 0.6, statistics_Button)
 exitButton = button.Button(size[0] / 2.5, size[1] * 0.8, exit_Button)
-
+backButton = button.Button(0, 0, back_Button)
 currentScene = "mainMenu"  #used so the program knows what screen to display
 
+backStack = list() #Stack for the back button
 
 #Scene creation for swapping through screens
-def mainMenuScene():
+def mainMenuScene(currentScene):
+    global backStack
     background = pygame.draw.rect(screen, defaultBackgroundColor, (0, 0, size[0], size[1]), 0)
     if startButton.draw(screen):  #checks if returned value of clicked is true or false
+        backStack.append(currentScene) # pushes the current scene onto the top of the stack
         currentScene = "playMenu"  #changes scene to the play menu
         print(currentScene)
     elif settingsButton.draw(screen):  #checks if returned value of clicked is true or false
@@ -41,19 +45,26 @@ def mainMenuScene():
         print(currentScene)
     elif exitButton.draw(screen):
         pygame.quit()
+    return currentScene
 
-
-def playMenuScene():
-    screen.fill(defaultBackgroundColor)
+def playMenuScene(currentScene):
+    global backStack
+    screen.fill(defaultBackgroundColor) # fills screen to remove all old scene
+    if backButton.draw(screen):
+        currentScene = backStack.pop()
+    return currentScene
 
 
 #game loop
 run = True
 while run:
+    print(backStack, "back stack")
     if currentScene == "mainMenu":
-        mainMenuScene()
+        print(currentScene)
+        currentScene = mainMenuScene(currentScene)
     elif currentScene == "playMenu":
-        playMenuScene()
+        print(currentScene)
+        currentScene = playMenuScene(currentScene)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
